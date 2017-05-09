bd8c9885c3e05241d3e2ff13dfcaf3fab9fa34a7
madguy02 <manishkakoti688@gmail.com>
2017-05-09 11:55:15 +0530

    added timestamp.js

---
+++ nodematrixserverlib/timestamp.js
@@ -0,0 +1,11 @@
+
+//turning time to millisecond posix time
+function AsTimeStamp(time) {
+    var time_to_ms = new Date().getTime();
+    return time_to_ms;
+}
+//turning millisecond posix time to Time(UTC)
+function UTCTime(milliseconds) {
+    var UTCTime_from_ms = new Date();
+    return UTCTime_from_ms;
+}
