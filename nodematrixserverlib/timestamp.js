5ac42ff6ce047b07a24081e8f34977a174e4ff3e
madguy02 <manishkakoti688@gmail.com>
2017-05-09 23:10:02 +0530

    changes done according to coacy

--- nodematrixserverlib/timestamp.js
+++ nodematrixserverlib/timestamp.js
@@ -1,13 +1,13 @@
 
 //turning time to millisecond posix time
 function AsTimeStamp(time) {
-    var time_to_ms = new Date().getTime();
-    return time_to_ms;
+    var TimeToMs = new Date().getTime();
+    return TimeToMs;
 }
 //turning millisecond posix time to Time(UTC)
 function UTCTime(milliseconds) {
-    var UTCTime_from_ms = new Date();
-    return UTCTime_from_ms;
+    var UTCTimeFromMs = new Date();
+    return UTCTimeFromMs;
 }
 
 exports.AsTimeStamp = AsTimeStamp;
